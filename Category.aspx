20f93f6d73cc1cdbf7af4e8be3f28ca8e58548ac
Diana Ghinea <97.diana@gmail.com>
2017-12-29 11:42:38 +0200

    Added a detailed page for the categories.
    Only displays the title of the category for now.

---
+++ Category.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Category.aspx.cs" Inherits="Category" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Label ID="CategoryName" runat="server" Text="Label"></asp:Label>
+</asp:Content>
+
---
+++ Category.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class Category : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Params["id"] == null) return;
+        int id = int.Parse(Request.Params["id"]);
+        string cerereSQL = "SELECT Name FROM Categories WHERE CategoryId=@pid";
+        SqlConnection cn = new SqlConnection(@"Data Source=LAPTOP-T2GBBU6T\SQLEXPRESS;Initial Catalog=Database;Integrated Security=True");
+        cn.Open();
+        try
+        {
+            SqlCommand cmd = new SqlCommand(cerereSQL, cn);
+            cmd.Parameters.AddWithValue("pId", id);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                CategoryName.Text = reader["Name"].ToString();
+            }
+            cn.Close();
+        }
+        catch (Exception exCMD)
+        {
+            Console.WriteLine(exCMD.Message);
+        }
+    }
+}
