befe40b06a6c3191ec9d9bb2b1d70e44f1edc639
Diana Ghinea <97.diana@gmail.com>
2017-12-29 12:56:14 +0200

    Added albums.
    The photos also belong to albums now. Each album has a details page.

---
+++ Album.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Album.aspx.cs" Inherits="Album" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Label ID="UserName" runat="server" Text="Label"></asp:Label> /
+    <asp:Label ID="AlbumName" runat="server" Text="Label"></asp:Label>
+    <br />
+    <asp:ListView ID="Photos" runat="server">
+        <ItemTemplate>
+            <div class="card" style="width: 20rem; display: inline-block;">
+                <asp:Image ID="Image1" runat="server" class="card-img-top" ImageUrl='<%# Eval("Url") %>' />
+                <div class="card-body">
+                    <asp:Label ID="Label1" class="card-text" runat="server"
+                        Text='<%# Eval("Description") %>' />
+                    <asp:Label ID="Label2" class="card-text" runat="server" 
+                        Text='<%# Eval("UploadDate") %>' />
+                    <br />
+                    <a href='Photo.aspx?id=<%# Eval("PhotoId") %>' class="card-link">View Details</a>
+                </div>
+            </div>
+        </ItemTemplate>
+    </asp:ListView>
+</asp:Content>
+
---
+++ Album.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Album : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Params["id"] == null) return;
+        int id = int.Parse(Request.Params["id"]);
+        this.fetchAlbumDetails(id);
+        this.fetchPhotos(id);
+    }
+
+    private void fetchAlbumDetails(int AlbumId)
+    {
+        string cerereSQL = "SELECT u.UserName,Name,Description,CreatedDate FROM Albums a,aspnet_Users u WHERE a.AlbumId=@pAlbumId AND a.UserId=u.UserId";
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString);
+        cn.Open();
+        try
+        {
+            SqlCommand cmd = new SqlCommand(cerereSQL, cn);
+            cmd.Parameters.AddWithValue("pAlbumId", AlbumId);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                AlbumName.Text = reader["Name"].ToString();
+                UserName.Text = reader["UserName"].ToString();
+            }
+            cn.Close();
+        }
+        catch (Exception exCMD)
+        {
+            Console.WriteLine(exCMD.Message);
+        }
+    }
+
+    private void fetchPhotos(int AlbumId)
+    {
+        string photosQuery =
+            "SELECT PhotoId,Url,Description,UploadDate FROM [Photos] " +
+            "WHERE AlbumId=@pAlbumId " +
+            "ORDER BY UploadDate DESC";
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString);
+        try
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand(photosQuery, cn);
+            cmd.Parameters.AddWithValue("pAlbumId", AlbumId);
+            SqlDataReader reader = cmd.ExecuteReader();
+            Photos.DataSource = reader;
+            Photos.DataBind();
+            cn.Close();
+        }
+        catch (Exception exCMD)
+        {
+            Console.WriteLine(exCMD.Message);
+        }
+    }
+}
